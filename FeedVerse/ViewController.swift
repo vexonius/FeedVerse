ef3408ff356cb5d00dcf391880ff32c0c58ad9e8
Tino Emer <emer.tino@feg.eu>
2022-04-02 02:34:36 +0200

    Added splash screen logo resource

--- FeedVerse/ViewController.swift
+++ FeedVerse/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        self.view.backgroundColor = .brown
     }
-
 
 }
 
