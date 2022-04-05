e6550d13cf7544de608240b2c0d894e24ab846b3
Tino Emer <emer.tino@feg.eu>
2022-04-05 10:33:00 +0200

    Created BaseViewController class

---
+++ FeedVerse/Presentation/Base/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  FeedVerse
+//
+//  Created by Tino Emer on 05.04.2022..
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+class BaseViewController: UIViewController {
+    let disposeBag = DisposeBag()
+}
